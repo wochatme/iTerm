afe647741dd47ac8d01391b4da189d7396982642
wochatme <wochatdb@gmail.com>
2024-08-02 19:07:50 -0700

    Init Commit

---
+++ win/MainFrm.h
@@ -0,0 +1,70 @@
+// MainFrm.h : interface of the CMainFrame class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+class CMainFrame : 
+	public CFrameWindowImpl<CMainFrame>, 
+	public CUpdateUI<CMainFrame>,
+	public CMessageFilter, public CIdleHandler
+{
+public:
+	DECLARE_FRAME_WND_CLASS(NULL, IDR_MAINFRAME)
+
+//	CView m_view;
+
+	virtual BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		if(CFrameWindowImpl<CMainFrame>::PreTranslateMessage(pMsg))
+			return TRUE;
+
+		return FALSE;
+	}
+
+	virtual BOOL OnIdle()
+	{
+		UIUpdateToolBar();
+		return FALSE;
+	}
+
+	BEGIN_UPDATE_UI_MAP(CMainFrame)
+	END_UPDATE_UI_MAP()
+
+	BEGIN_MSG_MAP(CMainFrame)
+		MESSAGE_HANDLER(WM_CREATE, OnCreate)
+		MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
+		CHAIN_MSG_MAP(CUpdateUI<CMainFrame>)
+		CHAIN_MSG_MAP(CFrameWindowImpl<CMainFrame>)
+	END_MSG_MAP()
+
+// Handler prototypes (uncomment arguments if needed):
+//	LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+//	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+//	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
+
+	LRESULT OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		//m_hWndClient = m_view.Create(m_hWnd, rcDefault, NULL, WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_HSCROLL | WS_VSCROLL, WS_EX_CLIENTEDGE);
+
+		// register object for message filtering and idle updates
+		CMessageLoop* pLoop = _Module.GetMessageLoop();
+		ATLASSERT(pLoop != NULL);
+		pLoop->AddMessageFilter(this);
+		pLoop->AddIdleHandler(this);
+
+		return 0;
+	}
+
+	LRESULT OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled)
+	{
+		// unregister message filtering and idle updates
+		CMessageLoop* pLoop = _Module.GetMessageLoop();
+		ATLASSERT(pLoop != NULL);
+		pLoop->RemoveMessageFilter(this);
+		pLoop->RemoveIdleHandler(this);
+
+		bHandled = FALSE;
+		return 1;
+	}
+};
